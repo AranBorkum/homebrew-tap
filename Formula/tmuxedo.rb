a253d4c271b264d4bd7a5c30fa7f796e9df51064
Aran Borkum <aran.borkum@kraken.tech>
2026-03-02 16:00:25 +0000

    Tmuxed v0.1.15

--- Formula/tmuxedo.rb
+++ Formula/tmuxedo.rb
@@ -1,8 +1,8 @@
 class Tmuxedo < Formula
   desc "A Cleaner, Modular Approach to Tmux Configuration"
   homepage "https://github.com/AranBorkum/tmuxedo"
-  url "https://github.com/AranBorkum/tmuxedo/archive/refs/tags/v0.1.14.tar.gz"
-  sha256 "c298cd4e8693570d80f10eebe81a8dc02f14fbdafe72486fd355b57790b04544"
+  url "https://github.com/AranBorkum/tmuxedo/archive/refs/tags/v1.1.15.tar.gz"
+  sha256 "c34510da90b777b398e9f7a3a055fb83fd888b8a50d5e18148e18d582e6412e6"
   license "MIT" # or your license
 
   depends_on "rust" => :build
